2e6b99dead7a474d04ffab884d39516a2f26cbd1
Jossie Quintero <quinterojosy@gmail.com>
2022-09-19 07:47:07 -0500

    Vista por tipo de roles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+        @if (Auth::user()->rol == 'RH')
+        <h1>SOY RH</h1>
+        @endif
+        @if (Auth::user()->rol == 'Empleado')
+        <h1>Soy Empleado</h1>
+        @endif
     </div>
 </div>
 @endsection
