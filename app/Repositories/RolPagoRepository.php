983d1885097f34e5181ee5f0441d1619a03a31e4
Jossie Quintero <quinterojosy@gmail.com>
2022-09-20 13:50:11 -0500

    Aporte IESS

--- app/Repositories/RolPagoRepository.php
+++ app/Repositories/RolPagoRepository.php
@@ -9,46 +9,50 @@ use App\Models\RolPago;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 
-final class RolPagoRepository {
+final class RolPagoRepository
+{
 
     private RolPago $model;
+    const APORTE_IESS = 0.0935;
 
-     /**
+    /**
      * Funcion constructuro da la clase
      *@param RolPago Rolpago
      *@return void
      *@author Jossie Quintero <quinterojosy@gmail.com>
      *@date septiembre 20th, 2022
      */
-    public function __construct(RolPago $RolPago) {
+    public function __construct(RolPago $RolPago)
+    {
         $this->model = $RolPago;
     }
 
-     /**
+    /**
      * Funcion que obtiene los Roles de pago
      *@param ninguno
      *@return Collection
      *@author Jossie Quintero <quinterojosy@gmail.com>
      *@date septiembre 20th, 2022
      */
-    public function all(){
+    public function all()
+    {
         return $this->model->all();
     }
 
-     /**
+    /**
      * Funcion que obtiene los roles con sus datos de usuario y empleado
      *@param ninguno
      *@return Collection
      *@author Jossie Quintero <quinterojosy@gmail.com>
      *@date septiembre 20th, 2022
      */
-    public function getAllRolWithUser(){
+    public function getAllRolWithUser()
+    {
         $UserAuth = User::find(auth()->user()->id);
-        $data= [];
-        if($UserAuth->rol == 'RH')
-        {
+        $data = [];
+        if ($UserAuth->rol == 'RH') {
             $allrolpago = $this->model->all();
-            foreach($allrolpago as $item){
+            foreach ($allrolpago as $item) {
                 $info = [];
                 $rolpago = new RolPago;
                 $rolpago = $item;
@@ -57,60 +61,56 @@ final class RolPagoRepository {
                 $datauser = new User;
                 $datauser = $dataempleado->user;
 
-                $info = ['rolpago_id'=>$rolpago->id,'name'=>$datauser->name,'apellidos'=>$datauser->apellidos, 'neto_pagar'=>$rolpago->neto_pagar,'fecha_creacion'=>$rolpago->created_at];
-                array_push($data,$info);
+                $info = ['rolpago_id' => $rolpago->id, 'name' => $datauser->name, 'apellidos' => $datauser->apellidos, 'neto_pagar' => $rolpago->neto_pagar, 'fecha_creacion' => $rolpago->created_at];
+                array_push($data, $info);
             }
 
             return $data;
-        }
-        else
-        {
+        } else {
             $empleado = $UserAuth->empleado;
             $roles = $empleado->rol_pago;
             return $roles;
         }
     }
 
-     /**
+    /**
      * Funcion que que edita un rol
      *@param int id
      *@return string
      *@author Jossie Quintero <quinterojosy@gmail.com>
      *@date septiembre 20th, 2022
      */
-    public function editRol(int $id){
+    public function editRol(int $id)
+    {
         return "Mensaje";
-
     }
 
-     /**
+    /**
      * Funcion que obtiene un rol de pago por su id
      *@param int id
      *@return RolPago
      *@author Jossie Quintero <quinterojosy@gmail.com>
      *@date septiembre 20th, 2022
      */
-    public function getRolPago($id){
+    public function getRolPago($id)
+    {
         $UserAuth = User::find(auth()->user()->id);
         $data = array();
-        if($UserAuth->rol = 'RH')
-        {
+        if ($UserAuth->rol = 'RH') {
             $rolpago = $this->model->find($id);
             $dataempleado = $rolpago->empleado;
             $datauser = $dataempleado->user;
 
             $info = [
-                'rolpago_id'=>$rolpago->id,
-                'name'=>$datauser->name,
-                'apellidos'=>$datauser->apellidos,
-                'neta_pagar'=>$rolpago->neto_pagar,
-                'fecha_creacion'=>$rolpago->created_at,
-                'balances'=> $rolpago->balance,
+                'rolpago_id' => $rolpago->id,
+                'name' => $datauser->name,
+                'apellidos' => $datauser->apellidos,
+                'neta_pagar' => $rolpago->neto_pagar,
+                'fecha_creacion' => $rolpago->created_at,
+                'balances' => $rolpago->balance,
             ];
             array_push($data, $info);
-
-        }
-        else if($UserAuth->rol = 'Empleado'){
+        } else if ($UserAuth->rol = 'Empleado') {
             $data = $this->model->where('empleado_id', $UserAuth->id)->get();
         }
         return $data;
--- app/Services/RolPagoService.php
+++ app/Services/RolPagoService.php
@@ -5,15 +5,13 @@ namespace App\Services;
 
 use App\Repositories\RolPagoRepository;
 use Illuminate\Database\Eloquent\Collection;
+use Ramsey\Uuid\Type\Decimal;
 
 final class RolPagoService{
 
     private RolPagoRepository $repository;
     public function __construct(RolPagoRepository $repo){
         $this->repository = $repo;
-    }
-    public function msg(){
-        return $this->repository->message();
     }
     public function all():Collection{
         return $this->repository->all();
@@ -23,5 +21,25 @@ final class RolPagoService{
     }
     public function getRolPago($id){
         return $this->repository->getRolPago($id);
+    }
+
+     /**
+     * Funcion que genera el total a pagar de un rol de pago
+     *@param $valores de balance y el sueldo
+     *@return decimal
+     *@author Jossie Quintero <quinterojosy@gmail.com>
+     *@date septiembre 20th, 2022
+     */
+    public function calcularSueldo(array $ingresos, array $egresos, $sueldo){
+        $aporteIESS = $sueldo * $this->repository->APORT_IESS;
+        $totalIngresos = 0;
+        $totalEgresos = 0;
+        foreach ($ingresos as $ingreso) {
+            $totalIngresos = $totalIngresos + $ingreso;
+        }
+        foreach ($egresos as $egreso) {
+            $totalEgresos = $totalEgresos + $egreso;
+        }
+        return ($sueldo - $aporteIESS) + $totalIngresos - $totalEgresos;
     }
 }
